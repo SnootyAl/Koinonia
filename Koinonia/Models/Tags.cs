5bb3ba1bee9b43e7f6b7ef4c67686e2de577e96a
Calum <calum.wilson@connect.qut.edu.au>
2019-08-28 18:18:58 +1000

    Created Tags
    Created Tag FIles

---
+++ Koinonia/Models/Tags.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Koinonia.Models
+{
+    public class Tags : ContentPage
+    {
+        public Tags()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
