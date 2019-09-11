0c5cc59f0ca7a41cd468657afdbb2b22ad133a3b
Alex <alex.raymond.mail@gmail.com>
2019-09-11 21:25:29 +1000

    TagsPage Converted
    Tentatively finished the bulk of the conversion to MVVM. Database still not working, but may still be a good spot to branch from

---
+++ Koinonia/ViewModel/TagsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Koinonia.ViewModel
+{
+    class TagsViewModel
+    {
+        private readonly IPageService _pageService;
+        public TagsViewModel(PageService pageService)
+        {
+            _pageService = pageService;
+        }
+    }
+}
