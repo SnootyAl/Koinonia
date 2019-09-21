4f8c9093402147e62dcab5f66ef6ca31ebb9927a
Calum <calum.wilson@connect.qut.edu.au>
2019-09-21 14:39:16 +1000

    Create Tags Page
    Created page for tag creation, with navigation from tags page. all the created tags are displayed in a list view and the basic UI for the new page has been created

---
+++ Koinonia/ViewModel/CreateTagsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Koinonia.ViewModel
+{
+    class CreateTagsViewModel
+    {
+    }
+}
--- Koinonia/ViewModel/TagsViewModel.cs
+++ Koinonia/ViewModel/TagsViewModel.cs
@@ -4,25 +4,41 @@ using System.Text;
 using MvvmHelpers;
 using Xamarin.Forms;
 using System.Windows.Input;
+using Koinonia.Views;
 
 namespace Koinonia.ViewModel
 {
     class TagsViewModel : BaseViewModel
     {
         private readonly IPageService _pageService;
+
+        public List<string> ListData { get; set; }
         public TagsViewModel(PageService pageService)
         {
             _pageService = pageService;
+
+            ListData = new List<string>()
+            {
+                "String A",
+                "String B",
+                "String C",
+                "String D"
+            };
+
+
         }
 
-        string name = "hello world";
+       
+        // Navigation for the tag creation page using create tag button page
 
-        public string Name {
-            get => name;
-            set {
-                SetProperty(ref name, value);
-                OnPropertyChanged(nameof(Name));
+        public Command ChangePage {
+            get {
+                return new Command(async () =>
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new CreateTagsPage());
+                });
             }
         }
+
     }
 }
